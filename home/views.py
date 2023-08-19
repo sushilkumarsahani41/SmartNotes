8fbefad06c09a627fe1614ceccb1c233b7dd80c6
Sushilkumar Sahani <dev@sahanisushilkumar.in>
2023-08-19 15:52:00 +0530

    Creating Notes App

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, "welcome.html", {'today': datetime.datetime.today()})
+
+
+@login_required(login_url="/admin")
+def auth(request):
+    return render(request, "auth.html",{})
