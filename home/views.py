4c82c29685a5c42f8835dd5760de7e059aa8183f
Sushilkumar Sahani <dev@sahanisushilkumar.in>
2023-08-19 15:32:40 +0530

    Template Creation

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def home(request):
+    return render(request, "welcome.html", {'today': datetime.datetime.today()})
